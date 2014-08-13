56935e6b824dc68266fe2a617b9b0e691c64b015
tschady <tschady@users.noreply.github.com>
2014-08-13 08:48:24 -0500

    add ruby-mode for god files

--- emacs/.emacs.d/settings/my-modes.el
+++ emacs/.emacs.d/settings/my-modes.el
@@ -23,6 +23,7 @@
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.god$" . ruby-mode))
 
 ;; Org Mode
 ; (setq org-agenda-files (quote ("~/Dropbox/TODO/101.org")))
