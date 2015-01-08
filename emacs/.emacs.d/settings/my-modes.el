84bedf22b15ca42a16474d53e91d50d4bc375d63
tschady <tschady@users.noreply.github.com>
2015-01-08 17:08:42 -0600

    add in new suffix for asciidoc

--- emacs/.emacs.d/settings/my-modes.el
+++ emacs/.emacs.d/settings/my-modes.el
@@ -6,6 +6,7 @@
 ;; Asciidoc
 (add-to-list 'auto-mode-alist (cons "\\.txt\\'" 'adoc-mode))
 (add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
+(add-to-list 'auto-mode-alist (cons "\\.asc\\'" 'adoc-mode))
 (add-to-list 'auto-mode-alist (cons "\\.asciidoc\\'" 'adoc-mode))
 (add-hook 'adoc-mode-hook (lambda() (buffer-face-mode t)))
 
