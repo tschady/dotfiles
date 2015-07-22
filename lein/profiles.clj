724c1e96db9043aeba8683ff81c5fb4f7d5252ce
tschady <tschady@users.noreply.github.com>
2015-07-22 05:53:50 -0500

    add style checker

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [
+{:user {:plugins [[lein-kibit "0.1.2"]
                   [lein-midje "3.1.3"]
                   [cider/cider-nrepl "0.8.1"]
                   [lein-exec "0.3.1"]]}}
