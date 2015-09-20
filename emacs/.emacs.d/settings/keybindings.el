cd5a09c923d89ec13cf35dcde21bcf670008ba74
tschady <tschady@users.noreply.github.com>
2015-09-20 09:42:43 -0500

    add in function to fix all whitespace in buffer

--- emacs/.emacs.d/settings/keybindings.el
+++ emacs/.emacs.d/settings/keybindings.el
@@ -6,5 +6,8 @@
 ;; Git
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;; buffer ops
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
 ;; jump mode
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
