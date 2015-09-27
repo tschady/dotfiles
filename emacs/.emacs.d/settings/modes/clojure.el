3eae94b1e6d87e30e86656262829602002fd67b2
tschady <tschady@users.noreply.github.com>
2015-09-27 08:26:37 -0500

    add and configure cider autocomplete

--- emacs/.emacs.d/settings/modes/clojure.el
+++ emacs/.emacs.d/settings/modes/clojure.el
@@ -15,3 +15,13 @@
     (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
+;; auto-complete
+(require 'ac-cider)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+(eval-after-load "auto-complete"
+  '(progn
+     (add-to-list 'ac-modes 'cider-mode)
+     (add-to-list 'ac-modes 'cider-repl-mode)))
