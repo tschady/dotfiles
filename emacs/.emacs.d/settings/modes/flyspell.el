4860cf50acc54e3da0e5a05c27a1d7ed1743c3f4
tschady <tschady@users.noreply.github.com>
2015-09-27 08:03:33 -0500

    add spellchecker using aspell

---
+++ emacs/.emacs.d/settings/modes/flyspell.el
@@ -0,0 +1,14 @@
+;; use aspell if installed
+(cond
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))))
+
+;; don't try and fix keywords
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+;; on/off modes
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+(dolist (hook '(change-log-mode-hook log-edit-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode -1))))
