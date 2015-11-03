31789aecf02fb0e08461f4aee0aea20839085b7c
tschady <tschady@users.noreply.github.com>
2015-11-03 08:51:32 -0600

    add shortcut for git-blame

--- emacs/.emacs.d/settings/keybindings.el
+++ emacs/.emacs.d/settings/keybindings.el
@@ -5,6 +5,7 @@
 
 ;; Git
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x \\") 'magit-blame)
 
 ;; buffer ops
 (global-set-key (kbd "C-c n") 'cleanup-buffer)
@@ -14,4 +15,3 @@
 (global-set-key (kbd "C-9") 'avy-goto-char-2)
 (global-set-key (kbd "M-g f") 'avy-goto-line)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
-
