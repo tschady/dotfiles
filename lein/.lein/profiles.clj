a5fb61408058a14933b9f204186af786b16bcc80
tschady <tschady@users.noreply.github.com>
2016-02-16 16:50:43 -0600

    Update versions, clean out obsolescence

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,7 +1,9 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
+{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                   [jonase/eastwood "0.2.1"]
                   [lein-bin "0.3.5"]
                   [lein-exec "0.3.1"]
                   [lein-kibit "0.1.2"]
-                  [lein-midje "3.1.3"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]]}}
+                  [lein-typed "0.3.5"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [org.clojure/core.typed "0.3.22"]]}}
