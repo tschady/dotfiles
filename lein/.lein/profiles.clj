9e5204f68fcb94569f9c7fa8c303247cfb92c5ab
tschady <tschady@users.noreply.github.com>
2016-07-17 06:59:47 -0500

    sync stable cider packages

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.12.0"]
                   [jonase/eastwood "0.2.1"]
                   [lein-bin "0.3.5"]
                   [lein-exec "0.3.1"]
                   [lein-kibit "0.1.2"]
                   [lein-typed "0.3.5"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.2.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/core.typed "0.3.22"]]}}
