20a1dbbf335924c330ab4544e2b9e9eea1c5e4eb
tschady <tschady@users.noreply.github.com>
2018-05-11 10:02:08 -0500

    Update for new cask targets

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,6 +4,13 @@
 ;
 ; I recently declared .emacs bankruptcy and started over after 15 yrs of cruft
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq debug-on-error t)
 
 ;;; initialization
