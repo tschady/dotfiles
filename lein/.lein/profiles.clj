9e0e58f65a7585e9d1e3bf92892589ce4f34e668
tschady <tschady@users.noreply.github.com>
2019-09-25 13:39:30 -0500

    Add secondary linter

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,2 +1,3 @@
 {:repl {:plugins [[cider/cider-nrepl "0.21.1"]]}
- :user {:plugins [[jonase/eastwood "0.3.5"]]}}
+ :user {:plugins [[jonase/eastwood "0.3.5"]
+                  [lein-cljfmt "0.6.4"]]}}
