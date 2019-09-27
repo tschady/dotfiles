7f0aeabdfc14f8fd217d74e1b78876cfe9c21a9b
tschady <tschady@users.noreply.github.com>
2019-09-27 04:17:56 -0500

    Add leiningen plugins

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,3 +1,5 @@
 {:repl {:plugins [[cider/cider-nrepl "0.21.1"]]}
  :user {:plugins [[jonase/eastwood "0.3.5"]
-                  [lein-cljfmt "0.6.4"]]}}
+                  [lein-cljfmt "0.6.4"]
+                  [lein-kibit "0.1.7"]
+                  [venantius/yagni "0.1.7"]]}}
