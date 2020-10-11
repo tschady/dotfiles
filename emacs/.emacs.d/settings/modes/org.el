4dfd150d9934160e8e492f13d6c0a851d3237b29
tschady <tschady@users.noreply.github.com>
2020-10-11 07:35:02 -0500

    minor orgmode changes

--- emacs/.emacs.d/settings/modes/org.el
+++ emacs/.emacs.d/settings/modes/org.el
@@ -1,6 +1,8 @@
-;; Org Mode
-; (setq org-agenda-files (quote ("~/Dropbox/TODO/101.org")))
-;'(org-code ((t :inherit (shadow fixed-pitch))))
-;'(org-block ((t :inherit (shadow fixed-pitch))))
-;'(org-block-background ((t :inherit fixed-pitch)))
-;'(org-table ((t :inherit fixed-pitch)))
+;;; Org Mode
+;; (setq org-agenda-files (quote ("~/Dropbox/TODO/101.org")))
+;; '(org-code ((t :inherit (shadow fixed-pitch))))
+;; '(org-block ((t :inherit (shadow fixed-pitch))))
+;; '(org-block-background ((t :inherit fixed-pitch)))
+;; '(org-table ((t :inherit fixed-pitch)))
+
+(setq org-journal-dir (quote "~/Dropbox/j/"))
