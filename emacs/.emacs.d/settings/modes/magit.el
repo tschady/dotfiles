9cd1efb75afee5ba79ca2053d71b1366599396cd
tschady <tschady@users.noreply.github.com>
2021-11-01 08:15:47 -0500

    Add magit-forge support

--- emacs/.emacs.d/settings/modes/magit.el
+++ emacs/.emacs.d/settings/modes/magit.el
@@ -1,3 +1,6 @@
 ;; auto-refresh magit window (has performance hit)
 (with-eval-after-load 'magit-mode
   (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
+
+(with-eval-after-load 'magit
+  (require 'forge))
